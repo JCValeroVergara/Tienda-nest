11112ecdc0be7b195c5cafde07d2844d2f3d1a7d
Juank_Valero <jvalero2009@gmail.com>
2024-03-24 18:27:27 -0500

    manejo de errores

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  //Manejo de errores
+  private readonly logger = new Logger('ProductsService')
 
 
   
@@ -25,8 +28,7 @@ export class ProductsService {
       return product;
       
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Error creating product');
+      this.handleDBExceptions(error);
     }
 
   }
@@ -46,4 +48,15 @@ export class ProductsService {
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Error creating product, check server logs');
+  }
+
 }
