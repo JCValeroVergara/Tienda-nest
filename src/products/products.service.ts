b638c5685a123666df4311872ddd5fab828d9138
Juank_Valero <jvalero2009@gmail.com>
2024-03-24 22:05:30 -0500

    Busqueda por Id, o por otro termino

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from '../common/dtos/pagination.dto';
+import {validate as isUUID} from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -43,10 +44,16 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id })
+  async findOne(term: string) {
+    let product: Product;
+    if(isUUID(term)){
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with ${term} not found`);
     }
     return product;
   }
