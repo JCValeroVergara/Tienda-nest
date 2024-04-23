d0e90a34d2b8aa2a528eddc6a5e17d73256cb272
Juan_Valero <juan.valero@selecu.net>
2024-04-23 13:25:26 -0500

    Feat, jwt strategy part 1, config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
+
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 
@@ -10,9 +12,24 @@ import { JwtModule } from '@nestjs/jwt';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    ConfigModule,
+
     TypeOrmModule.forFeature([User]),
 
     PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async ( configService : ConfigService) => {
+        // console.log('JWT_SECRET', configService.get("JWT_SECRET"));
+        // console.log('JWT_SECRET', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        };
+      },
+    }),
 
     // JwtModule.register({
     //   secret: process.env.JWT_SECRET,
---
+++ src/auth/interfaces/jwt.interface.ts
@@ -0,0 +1,7 @@
+
+
+export interface JwtPayload {
+    email: string;
+
+    //TODO: Add more fields
+}
