20d10dbd2ca0801a42184f90e524ce156796307a
Juank_Valero <jvalero2009@gmail.com>
2024-07-28 23:13:16 -0500

    feat: Add serve-static module for serving static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
@@ -22,6 +24,12 @@ import { FilesModule } from './files/files.module';
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+
+    
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
     }),
 
     ProductsModule,
